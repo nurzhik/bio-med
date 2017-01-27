1832c4ace0efb5bab5b3c7b03ed72d51869c3fa8
nurzhik <nurzhananuarbek@mail.ru>
2017-01-27 17:11:43 +0600

    test8

--- js/app.js
+++ js/app.js
@@ -151,12 +151,35 @@ jQuery(document).ready(function ($) {
     });
 
 
+
 })
 
-
-
-
-
-
+jQuery(document).ready(function() { 
+   
+    var overlay = $('#overlay');
+    var open_modal = $('.open_modal'); 
+    var close = $('.modal_close, #overlay'); 
+    var modal = $('.modal_div'); 
 	
+     open_modal.click( function(event){ 
+         event.preventDefault(); 
+         var div = $(this).attr('href'); 
+         overlay.fadeIn(400,
+             function(){
+                 $(div) 
+                     .css('display', 'block') 
+                     .animate({opacity: 1, }, 200);
+         });
+     });
 
+     close.click( function(){-
+            modal 
+             .animate({opacity: 0, }, 200, 
+                 function(){ 
+                     $(this).css('display', 'none');
+                     overlay.fadeOut(400); 
+                 }
+             );
+     });
+});
+   
